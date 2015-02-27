6fd7b805980015748749ba04c110a3d74516a84e
Orest Savchak <hydr0@list.ru>
2015-02-27 12:04:19 +0200

    Initial Commit

---
+++ IPAIconGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IPAIconGenerator
+//
+//  Created by Orest Savchak on 2/27/15.
+//  Copyright (c) 2015 Orest&Vadym. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IPAIconGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IPAIconGenerator
+//
+//  Created by Orest Savchak on 2/27/15.
+//  Copyright (c) 2015 Orest&Vadym. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
